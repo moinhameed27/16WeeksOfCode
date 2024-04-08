52041ae2d36602d09556da0611bf02f991be6a65
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-08 13:52:47 +0500

    Number of Students Unable to Eat Lunch

--- Week 3/Day 4/Number of Students Unable to Eat Lunch.cpp
+++ Week 3/Day 4/Number of Students Unable to Eat Lunch.cpp
@@ -1,3 +1,4 @@
+// Way - I --> Built in Functions (Rotate and Erase)
 class Solution
 {
 public:
@@ -27,5 +28,33 @@ public:
                 break;
         }
         return unableToEat;
+    }
+};
+
+// Way - II (Counter)
+class Solution
+{
+public:
+    int countStudents(vector<int> &students, vector<int> &sandwiches)
+    {
+        int n = students.size();
+        int arr[2] = {0};
+        // arr[0] = Count of students who like circular sandwiches
+        // arr[1] = Count of students who like square sandwiches
+
+        // Counting the students according to their preference
+        for (int x : students)
+            arr[x]++;
+    
+        // Traversing the sandwiches array, decrementing the sandwiches according to preference and if there are no students of such
+        // preference then return the remaining students because there's no one who can remove that sandwich from the top of stack
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[sandwiches[i]] == 0)
+                return n - i;
+            arr[sandwiches[i]]--;
+        }
+
+        return 0;
     }
 };
